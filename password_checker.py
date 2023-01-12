920e5323ae694af4dcfc8ae17763faf96e87543d
MatthewRoe01 <109381707+MatthewRoe01@users.noreply.github.com>
2023-01-12 01:42:52 -0700

    Finish password_checker.py
    This password_checker uses a for loop to iterate through the password
    inputed as a list and then checks each character to see if it's an uppercase,
    lowercase, or special character. It also checks the length of the password
    If the password is missing any of those characters or it is less than 9
    characters in length, the program results in an error telling the user
    it's password is weak.

--- password_checker.py
+++ password_checker.py
@@ -1,3 +1,29 @@
 import string
 password = list(input('Enter a password: '))
+count = 0
+qualifications = ["lowercase", "uppercase", "digit", "special"]
+for char in password:
+    count = count+1
+    if char in string.ascii_lowercase:
+        if 'lowercase' in qualifications:
+            qualifications.remove("lowercase")
+    elif char in string.ascii_uppercase:
+        if 'uppercase' in qualifications:
+            qualifications.remove("uppercase")
+    elif char in string.digits:
+        if 'digit' in qualifications:
+            qualifications.remove("digit")
+    else:
+        if 'special' in qualifications:
+            qualifications.remove("special")
+if count < 9:
+        print('Your password must be at least 9 characters.')
+if qualifications == []:
+    print('Success, you have a strong password')
+else:
+    qualneeded = ", ".join(qualifications)
+    print('Error: Enter a stronger password. You must include the following characters: ', qualneeded)
+
+
+    
 

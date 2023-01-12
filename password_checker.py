f158e9ed374294297b12b0da59327242c46e0237
MatthewRoe01 <109381707+MatthewRoe01@users.noreply.github.com>
2023-01-12 01:53:36 -0700

    Add a counter for each of the character types. Adds a counter for the amount of uppercase, lowercase, digits, and special characters. I'd like to mention that a tutorial by Travis Media helped me along with this project.

--- password_checker.py
+++ password_checker.py
@@ -2,18 +2,26 @@ import string
 password = list(input('Enter a password: '))
 count = 0
 qualifications = ["lowercase", "uppercase", "digit", "special"]
+lowercase = 0
+uppercase = 0
+digit = 0
+special = 0
 for char in password:
     count = count+1
     if char in string.ascii_lowercase:
+        lowercase+= 1
         if 'lowercase' in qualifications:
             qualifications.remove("lowercase")
     elif char in string.ascii_uppercase:
+        uppercase += 1 
         if 'uppercase' in qualifications:
             qualifications.remove("uppercase")
     elif char in string.digits:
+        digit += 1
         if 'digit' in qualifications:
             qualifications.remove("digit")
     else:
+        special += 1
         if 'special' in qualifications:
             qualifications.remove("special")
 if count < 9:
@@ -23,6 +31,11 @@ if qualifications == []:
 else:
     qualneeded = ", ".join(qualifications)
     print('Error: Enter a stronger password. You must include the following characters: ', qualneeded)
+print('<------------------->')
+print('Amount of lowercase letters:', lowercase)
+print('Amount of uppercase letters:', uppercase)
+print('Amount of digits:', digit)
+print('Amount of special characters:', special)
 
 
     
